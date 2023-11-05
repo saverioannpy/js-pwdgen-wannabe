5e6e5d70970ff37d649fe69da08e44bfde2f5838
saverioannpy <saverio.annunziata@outlook.com>
2023-11-05 20:14:58 +0100

    first commit

---
+++ myscript.js
@@ -0,0 +1,13 @@
+'use strict';
+//Input Nome
+const nomeInput = prompt('Inserisci il tuo nome: ');
+console.log(nomeInput);
+//Input Cognome
+const cognomeInput = prompt('Inserisci il tuo cognome: ');
+console.log(cognomeInput);
+//Input Colore Preferito
+const coloreInput = prompt('Inserici il tuo colore preferito: ');
+console.log(coloreInput);
+// Passaggio dati forniti in input all' HTML
+elementoPassword.innerHTML = nomeInput + cognomeInput + coloreInput + "21";
+
